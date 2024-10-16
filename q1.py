d5faa9d23d700bda59c8406ec1da9004ce20f53a
PRIYANSHU <shrivastava0820@gmail.com>
2024-10-16 22:11:33 +0530

    Update q1.py

--- q1.py
+++ q1.py
@@ -7,27 +7,27 @@ print(df)
 X = df.iloc[:,:].values.reshape(-1, 1)
 y = df['price'].values
 
-# Linear regression using gradient descent
+
 def linear_regression(X, y, learning_rate=0.00000001, iterations=1000) :
-    w= 0  # Initialize slope
-    b = 0  # Initialize intercept
-    n = float(len(X))  # Number of data points
+    w= 0  
+    b = 0  
+    n = float(len(X)) 
 
     for i in range(iterations):
-        y_pred = w* X + b  # Predicted value of y
-        D_w = (-2/n) * sum(X * (y - y_pred))  # Derivative of m
-        D_b = (-2/n) * sum(y - y_pred)        # Derivative of c
-        w = w - learning_rate * D_w  # Update m
-        b = b - learning_rate * D_b  # Update c
+        y_pred = w* X + b  
+        D_w = (-2/n) * sum(X * (y - y_pred)) 
+        D_b = (-2/n) * sum(y - y_pred)       
+        w = w - learning_rate * D_w 
+        b = b - learning_rate * D_b  
 
     return w, b
 
-# Train the model
+
 w, b = linear_regression(X, y)
 print(f"Optimal slope (w): {np.round(w,2)}")
 print(f"Optimal intercept (b): {np.round(b,2)}")
 
-# Make predictions
+
 y_pred = w * X + b
 
 print(np.round(y_pred,-1))
